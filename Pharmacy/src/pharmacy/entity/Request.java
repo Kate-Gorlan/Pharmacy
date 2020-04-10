00d159c4e9138ac4062715caa104611dfcac5d6c
Lazjn <Lazjn@MICROSOFT-PC>
2020-04-10 12:01:14 +0300

    request class

---
+++ Pharmacy/src/pharmacy/entity/Request.java
@@ -0,0 +1,16 @@
+package pharmacy.entity;
+
+public class Request {
+
+    private static final long serialVersionUID = 1L;
+
+    private Client client;
+    
+    private Medicament medicament;
+    
+    private int quantity;
+    
+    private boolean status;
+    
+    //private Picture
+}
