c92e1d6e0a9ca58913d14c044abb7ab6bda1a05c
Lazjn <Lazjn@MICROSOFT-PC>
2020-04-14 17:31:12 +0300

    request class/mapper/dao/service

---
+++ Pharmacy/src/pharmacy/dao/mybatis/RequestMapper.java
@@ -0,0 +1,37 @@
+package pharmacy.dao.mybatis;
+
+import java.util.List;
+
+import org.mybatis.spring.support.SqlSessionDaoSupport;
+
+import pharmacy.dao.RequestDao;
+import pharmacy.entity.Request;
+
+public class RequestMapper extends SqlSessionDaoSupport implements RequestDao{
+
+    @Override
+    public void create(Request obj) {
+        getSqlSession().insert("pharmacy.dao.RequestDao.create", obj);
+    }
+
+    @Override
+    public Request read(Long id) {
+        return getSqlSession().selectOne("pharmacy.dao.RequestDao.read", id);
+    }
+
+    @Override
+    public void update(Request obj) {
+        getSqlSession().update("pharmacy.dao.RequestDao.update", obj);
+    }
+
+    @Override
+    public void delete(Long id) {
+        getSqlSession().delete("pharmacy.dao.RequestDao.delete", id);
+    }
+
+    @Override
+    public List<Request> findAll() {
+        return getSqlSession().selectList("pharmacy.dao.RequestDao.findAll");
+    }
+
+}
--- Pharmacy/src/pharmacy/entity/Request.java
+++ Pharmacy/src/pharmacy/entity/Request.java
@@ -1,16 +1,57 @@
 package pharmacy.entity;
 
-public class Request {
+public class Request extends Entity {
 
     private static final long serialVersionUID = 1L;
 
     private Client client;
-    
+
     private Medicament medicament;
-    
+
     private int quantity;
-    
+
     private boolean status;
-    
-    //private Picture
+
+    private byte[] img;
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public Medicament getMedicament() {
+        return medicament;
+    }
+
+    public void setMedicament(Medicament medicament) {
+        this.medicament = medicament;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public byte[] getImg() {
+        return img;
+    }
+
+    public void setImg(byte[] img) {
+        this.img = img;
+    }
+
 }
